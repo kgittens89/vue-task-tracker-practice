3eeb3a5ab72ea7c2ddaf63f4fda22baac93c6850
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-04-14 11:55:42 -0500

    Add Task with dblclick & click event methods

---
+++ src/components/Tasks.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div :key="task.id" v-for="task in tasks">
+            <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" />
+        </div>
+    </div>
+</template>
+<script>
+import Task from './Task.vue';
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Tasks',
+    props: {
+        tasks: Array,
+    },
+    components: { 
+        Task
+    },
+    emit: ['delete-task', 'toggle-reminder'],
+}
+</script>
+<style>
+    
+</style>
