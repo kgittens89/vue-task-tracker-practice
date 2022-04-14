0f8966043b217f5d83b747cc066b03bc55427989
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-04-14 11:36:56 -0500

    Add Header component with Button w/ onClick.

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+        <button @click="onClick()" class="btn" :style="{background: color}" >{{text}}</button>
+</template>
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods: {
+        onClick() {
+            console.log('click')
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template lang="">
+    <header>
+        <h1> {{title}} </h1>
+        <Button text="Add Task" color="green" />
+    </header>
+</template>
+<script>
+import Button from './Button.vue';
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "Header",
+    props: {
+        title: String
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
