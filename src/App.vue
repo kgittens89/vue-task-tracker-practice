6a14d96e084a3d87ce7698960044810f813ad8f1
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-04-17 17:40:30 -0500

    Add full CRUD capabilities w/ fake json-server

--- src/App.vue
+++ src/App.vue
@@ -30,18 +30,43 @@ export default {
     this.tasks = await this.fetchTasks()
   },
   methods: {
-    deleteTask(id) {
+    async deleteTask(id) {
       if(confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id)
+        const res = await fetch(`http://localhost:3000/tasks/${id}`, {
+          method: 'DELETE'
+        })
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
       }
     },
-    toggleReminder(id) {
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updateTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+      const res = await fetch(`http://localhost:3000/tasks/${id}`, {
+        method: 'PUT',
+        headers: { 
+          'Content-type': 'application/json' 
+        },
+        body: JSON.stringify(updateTask)
+      })
+      const data = await res.json()
+
       this.tasks = this.tasks.map((task) => {
-      return task.id === id ? {...task, reminder: !task.reminder} : task
+      return task.id === id ? {...task, reminder: data.reminder} : task
       })
     },
-    addTask(task) {
-      this.tasks = [...this.tasks, task]
+    async addTask(task) {
+      const res = await fetch('http://localhost:3000/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+          body: JSON.stringify(task)
+      })
+      const data = await res.json()
+      console.log(res)
+      console.log(data)
+      this.tasks = [...this.tasks, data]
     },
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
